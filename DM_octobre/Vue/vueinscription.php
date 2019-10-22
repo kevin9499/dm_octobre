e4dda2a45ef99997d5353bdce374fb0979b46812
kevin9499 <46714122+kevin9499@users.noreply.github.com>
2019-10-22 17:42:04 +0200

    Add files via upload

---
+++ DM_octobre/Vue/vueinscription.php
@@ -0,0 +1,11 @@
+<center>
+<form class="form-group" action="" method="post">
+<table>
+<tr><td class="td1">Nom</td><td><input class="form-control" type="text" name="nom"></td></tr>
+<tr><td class="td1">Prenom</td><td><input class="form-control" type="text" name="prenom"></td></tr>
+<tr>
+            <td class="td1"><input class="button8 btn btn-primary" type="submit" name="sinscrire" value="S'inscrire"></td>
+</tr>
+</table>
+</form>
+</center>
