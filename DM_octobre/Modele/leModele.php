c158e3d29e782a8b83cb2e264f527f285b85ebe6
kevin9499 <46714122+kevin9499@users.noreply.github.com>
2019-10-24 15:15:35 +0200

    Add files via upload

--- DM_octobre/Modele/leModele.php
+++ DM_octobre/Modele/leModele.php
@@ -134,6 +134,87 @@ public function selectObjet()
         return $result;
     }
 }
+public function selectSelfObjet()
+{
+    if($this->unPdo!=null)
+    {
+        $requete = "select * from objet where  id_enfant = :id_enfant;";
+        $donnees = array(":id_enfant"=>$_SESSION['id_enfant']);                    
+        $select = $this->unPdo->prepare($requete);
+        $select->execute($donnees);
+        $result = $select->fetchAll();
+        return $result;
+    }
+}
+public function selectCout($id_objet)
+{
+    if($this->unPdo!=null)
+    {
+        $requete = "select point from objet where  id_objet = :id_objet;";
+        $donnees = array(":id_objet"=>$id_objet);                    
+        $select = $this->unPdo->prepare($requete);
+        $select->execute($donnees);
+        $result = $select->fetch();
+        return $result;
+    }
+}
+public function selectVente()
+{
+    if($this->unPdo!=null)
+    {
+        $requete = 'select * from vente where type = "vente"';
+        $select = $this->unPdo->prepare($requete);
+        $select->execute();
+        $result = $select->fetchAll();
+        return $result;
+    }
+}
+public function selectTroc()
+{
+    if($this->unPdo!=null)
+    {
+        $requete = 'select * from vente where type = "troc"';
+        $select = $this->unPdo->prepare($requete);
+        $select->execute();
+        $result = $select->fetchAll();
+        return $result;
+    }
+}
+public function updateObjet($tab, $id)
+{
+    if($this->unPdo!=null)
+    {
+            $requete ="update objet set type = :type ,id_enfant = :id_enfant where id_objet = :id_objet;";
+            $donnees = array(":type"=>$tab['type'],":id_enfant"=>$_SESSION['id_enfant'],":id_objet"=>$id);
+            $insert = $this->unPdo->prepare($requete);
+            $insert->execute($donnees);
+            $donnees = array(":id_objet"=>$tab['id_objet'],":type"=>$tab['type'],":id_enfant"=>$id);
+            $insert2 = $this->unPdo->prepare($requete);
+            $insert2->execute($donnees);
+
+    }
+}
+public function updateObjet2($tab, $id)
+{
+    if($this->unPdo!=null)
+    {
+            $requete ="update objet set type = :type ,id_enfant = :id_enfant where id_objet = :id_objet;";
+            $donnees = array(":type"=>$tab['type'],":id_enfant"=>$_SESSION['id_enfant'],":id_objet"=>$id);
+            $insert = $this->unPdo->prepare($requete);
+            $insert->execute($donnees);
+            $donnees = array(":id_objet"=>$tab['id_objet'],":id_enfant"=>$id);
+            $insert2 = $this->unPdo->prepare($requete);
+            $insert2->execute($donnees);
+            $requete ="update enfant set point = :point  where id_enfant = :id_enfant";
+            $donnees = array(":point"=>-$tab['point'],":id_enfant"=>$_SESSION['id_enfant']);
+            $insert3 = $this->unPdo->prepare($requete);
+            $insert3->execute($donnees);
+            $donnees = array(":point"=>+$tab['point'],":id_enfant"=>$id);
+            $insert4 = $this->unPdo->prepare($requete);
+            $insert4->execute($donnees);
+
+    }
+}
 
 }
 ?>
